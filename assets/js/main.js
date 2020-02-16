87b46718a28968652bcc0f3b1d71d82b31273a55
Tom Pinchen <t_pinchen@yahoo.co.uk>
2020-02-16 15:11:59 +0000

    Add bespoke tour and guides page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,47 @@
-$(document).ready( function() {
-
+$(document).ready(function() {
   function setImgHeight() {
     var windowHeight = $(window).height();
     var url = window.location.href;
     var textWrapperHeight = $(".text-wrapper").height();
 
     // Looks for specific text in url to apply half height hero image on non-homepage
-    if ( url.match( /about/g) || url.match( /\/tours/g) || url.match( /testimonials/g) || url.match( /contact/g) || url.match( /faqs/g) || url.match( /terms/g) || url.match( /thankyou/g)  ) {
-      
-      $(".js-wrapper").css("height", (windowHeight / 1.7));
+    if (
+      url.match(/about/g) ||
+      url.match(/guides/g) ||
+      url.match(/\/tours/g) ||
+      url.match(/testimonials/g) ||
+      url.match(/contact/g) ||
+      url.match(/faqs/g) ||
+      url.match(/terms/g) ||
+      url.match(/thankyou/g)
+    ) {
+      $(".js-wrapper").css("height", windowHeight / 1.7);
     } else {
-      
-      $(".js-wrapper").css("height", windowHeight );
+      $(".js-wrapper").css("height", windowHeight);
       //Recalulates margins on window resize
-      $(".text-wrapper").css("margin-top", ((windowHeight / 2)- (textWrapperHeight/2)));
-      $(".text-wrapper").css("margin-bottom", ((windowHeight / 2)- (textWrapperHeight/2)));
+      $(".text-wrapper").css(
+        "margin-top",
+        windowHeight / 2 - textWrapperHeight / 2
+      );
+      $(".text-wrapper").css(
+        "margin-bottom",
+        windowHeight / 2 - textWrapperHeight / 2
+      );
     }
   }
 
   // Navbar opacity layer
-  $(".navbar-default .navbar-toggle").click(function(){
+  $(".navbar-default .navbar-toggle").click(function() {
     $("#homepage_hero").toggleClass("opacity");
     $("#about_hero").toggleClass("opacity");
     $("#faq_hero").toggleClass("opacity");
-  })
-   
+  });
+
   //window resizing
 
-  $(window).on('resize', function() {
+  $(window).on("resize", function() {
     setImgHeight();
   });
 
-
   setImgHeight();
-
 });
-
